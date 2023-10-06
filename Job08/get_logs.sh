5d3f1296f93176697bf488beb50a5113c47a3ae1
tristan-bayol <tristan.bayol@laplateforme.io>
2023-10-06 10:40:08 +0200

    modif script job09

--- Job08/get_logs.sh
+++ Job08/get_logs.sh
@@ -2,6 +2,9 @@
 #Definition des variables
 current_date=$(date +\%d-\%m-\%Y-\%H:\%M)
 
+#Evite de taper le chemin absolu pour cron à chaque action
+cd ~/Documents/Git/shell-exe/Job08/
+
 #Suppression des fichiers précédemments créer
 #L'etoile permet de supprimer tous les fichiers contenant number_connection
 rm number_connection*
@@ -10,4 +13,4 @@ rm number_connection*
 (last && last | grep titou -wc) > number_connection-$current_date
 
 # Création de l'archive dans le dossier Backup
-tar -cvf /home/titou/Documents/Git/shell-exe/Job08/Backup/number_connection-$current_date.tar number_connection-$current_date
+tar -cf ~/Documents/Git/shell-exe/Job08/Backup/number_connection-$current_date.tar number_connection-$current_date
---
+++ Job09/accessrights.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#Chemin du script pour cron
+cd ~/Documents/Git/shell-exe/Job09
+
+#Definitions des variables
+Userlist="/home/titou/Téléchargements/Shell_Userlist.csv"
+
+#Supprimer tout les espaces dans le fichier
+sed -i 's/ //g' $Userlist
+#Commande a la fonction read de separer les infos à chaque virgule
+IFS=","
+	#Création de la fonction
+	create_user()
+{
+#Lire le fichdier Csv
+while read id prenom nom mdp role
+	do
+		#-m créer le home de l'utilisateur
+		#-p permet de créer l'utilisateur avec le mdp
+		echo sudo useradd "$prenom-$nom" -m -p $mdp -u $id --badname
+
+#Utilise mon Csv à partir de la ligne 2
+done < $Userlist | tail -n +2
+}
+create_user $id $prenom $nom $mdp $role
+echo $role
+#	if [ $role = "Admin" ]; then
+#		echo sudo usermod -aG sudo "$prenom-$nom"
+#	fi
+
